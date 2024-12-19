24cc0b55148bbb867f67b2541bd42ed4859e471f
kitsune3 <kitsune3@github.com>
2024-12-19 13:46:29 +0900

    update pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if type "docker" > /dev/null 2>&1; then
-    docker run --rm -v ./docs:/documents/ -v ./output:/documents/output asciidoctor/docker-asciidoctor $CMD
+    docker run --rm -v ./docs:/documents/ -v ./output:/documents/output asciidoctor/docker-asciidoctor asciidoctor-pdf -a scripts=cjk -a pdf-theme=default-with-fallback-font -a attribute-missing=warn --failure-level=WARN -D output *.adoc
     docker run --rm -v ./docs:/documents/ -v ./output:/documents/output asciidoctor/docker-asciidoctor asciidoctor -a scripts=cjk -a pdf-theme=default-with-fallback-font -a attribute-missing=warn --failure-level=WARN -D output *.adoc
     docker run --rm -v ./docs:/documents/ -v ./output:/documents/output pandoc/latex /documents/output/resume.html -o /documents/output/resume.docx
 else
