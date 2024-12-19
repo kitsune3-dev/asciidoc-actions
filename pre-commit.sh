802bf055a0e29ca664a5fe8f0e3af5a8719888f0
kitsune3 <kitsune3@github.com>
2024-12-19 13:34:59 +0900

    implment pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-CMD="asciidoctor-pdf -a scripts=cjk -a pdf-theme=default-with-fallback-font -a attribute-missing=warn --failure-level=WARN -D output *.adoc"
-
 if type "asciidoctor-pdf" > /dev/null 2>&1; then
+    asciidoctor-pdf -a scripts=cjk -a pdf-theme=default-with-fallback-font -a attribute-missing=warn --failure-level=WARN -D output *.adoc
     $CMD
 elif type "docker" > /dev/null 2>&1; then
     docker run --rm -v ./docs:/documents/ -v ./output:/documents/output asciidoctor/docker-asciidoctor $CMD
 else
     echo "asciidoctor-pdf not found"
     rm *.pdf
+    rm *.docx
     exit 1
 fi
 
 if [ $? -ne 0 ]; then
     rm *.pdf
+    rm *.docx
     exit 1
 fi
