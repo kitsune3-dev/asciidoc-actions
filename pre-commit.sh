d1db9911107b52f16f8e93afef50d4daea17fcd1
kitsune <8329802+kitsune3-dev@users.noreply.github.com>
2024-12-19 13:53:39 +0900

    Feature/implment#2 (#3)
    * implment pre-commit
    
    * implment pre-commit
    
    * check pre-commit
    
    * fix clean output dir
    
    * check pre-commit
    
    * fix-precommit
    
    * check pre-commit
    
    * update pre-commit
    
    * check pre-commit
    
    ---------
    
    Co-authored-by: kitsune3 <kitsune3@github.com>

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-CMD="asciidoctor-pdf -a scripts=cjk -a pdf-theme=default-with-fallback-font -a attribute-missing=warn --failure-level=WARN -D output *.adoc"
-
-if type "asciidoctor-pdf" > /dev/null 2>&1; then
-    $CMD
-elif type "docker" > /dev/null 2>&1; then
-    docker run --rm -v ./docs:/documents/ -v ./output:/documents/output asciidoctor/docker-asciidoctor $CMD
+if type "docker" > /dev/null 2>&1; then
+    docker run --rm -v ./docs:/documents/ -v ./output:/documents/output asciidoctor/docker-asciidoctor asciidoctor-pdf -a scripts=cjk -a pdf-theme=default-with-fallback-font -a attribute-missing=warn --failure-level=WARN -D output *.adoc
+    docker run --rm -v ./docs:/documents/ -v ./output:/documents/output asciidoctor/docker-asciidoctor asciidoctor -a scripts=cjk -a pdf-theme=default-with-fallback-font -a attribute-missing=warn --failure-level=WARN -D output *.adoc
+    docker run --rm -v ./docs:/documents/ -v ./output:/documents/output pandoc/latex /documents/output/resume.html -o /documents/output/resume.docx
 else
     echo "asciidoctor-pdf not found"
-    rm *.pdf
+    rm ourtput/*.pdf
+    rm output/*.docx
+    rm output/*.html
     exit 1
 fi
 
 if [ $? -ne 0 ]; then
-    rm *.pdf
+    rm output/*.pdf
+    rm output/*.docx
+    rm output/*.html
     exit 1
 fi
